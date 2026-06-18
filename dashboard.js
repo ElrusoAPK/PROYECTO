d64e8324727d50eee781372f6eebd835c5fcd7db
Diana Laura <omarromanperez@gmail.com>
2026-06-17 19:25:01 -0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -13,7 +13,7 @@ CARGAR CSV
 document.addEventListener("DOMContentLoaded", () => {
 
 ```
-Papa.parse("SSNMX_catalogo_csv", {
+Papa.parse("SSNMX_catalogo_19000601_20260602.csv", {
 
     download: true,
     header: true,
